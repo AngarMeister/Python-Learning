c37cf81655960c3503e96cc10ba576c82a7dd4e3
AngmarMeister <shamlov.nikita@gmail.com>
2024-11-26 19:07:06 +0200

    Added arithmetic operators

---
+++ Python Arithmetic Operators/Arithmetic_operators.py
@@ -0,0 +1,11 @@
+# Operators
+x = 5
+y = 5
+
+print(x + y)  # Сложение -> 10
+print(x - y)  # Вычет -> 0
+print(x * y)  # Умножение -> 25
+print(x / y)  # Divide -> 1
+print(x % y)  # Modulus, остаток 7/5 remain 2
+print(x ** y)  # Power -> 3125
+print(x // y)  # Floor division -> 1. 17 / 5 = 3. Floor
