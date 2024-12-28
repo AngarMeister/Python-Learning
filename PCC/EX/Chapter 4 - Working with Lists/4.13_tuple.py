47e3f6cc91876003e98feaa9cc0ffa8e208e610c
AngarMeister <shamlov.nikita@gmail.com>
2024-12-28 15:38:26 +0200

    Chapter 4: 4.13 tuple

---
+++ PCC/EX/Chapter 4 - Working with Lists/4.13_tuple.py
@@ -0,0 +1,7 @@
+food = ("Sosiski", "Mazik", "Apple", "Totrila", "Heheh")
+for item in food:
+    print(item)
+print("Oh no, the menu has changed!")
+food = ("Sosiski", "Love", "Apple", "Totrila", "Mehehge")
+for item in food:
+    print(item)
